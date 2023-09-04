be3e2a43e3981b8164c29e1c37938985e83e8dca
Muaz Ashraf <muazashrafnet@gmail.com>
2023-09-04 20:54:35 +0500

    new

--- src/components/WeddingCard.jsx
+++ src/components/WeddingCard.jsx
@@ -5,6 +5,7 @@ import {
   CardActions,
   CardContent,
   CardHeader,
+  Stack,
   Typography,
 } from "@mui/material";
 import React from "react";
@@ -13,21 +14,43 @@ const WeddingCard = () => {
   return (
     <Card
       variant="outlined"
-      sx={{ borderRadius: "0.1rem", bgcolor: "lightpink" }}
+      sx={{
+        borderRadius: "0.1rem",
+        bgcolor: "lightpink",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
     >
       <CardHeader title="Wedding Invitation" />
-      <CardContent>
+      <CardContent
+        sx={{
+          textAlign: "center",
+
+          display: "flex",
+          flexDirection: "column",
+          gap: "2rem",
+        }}
+      >
         <Typography>
           We cordially invite you to the wedding celebration of
         </Typography>
-
-        <Typography>Date</Typography>
-        <Typography>Time</Typography>
-        <Typography>Venue</Typography>
+        <Typography fontSize={"2rem"}>𝓣𝓤𝓑𝓐 & 𝓜𝓤𝓐𝓩</Typography>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography>Date</Typography>
+          <Typography>24 December 2023</Typography>
+        </Stack>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography>Time</Typography> <Typography>1 to 4 pm</Typography>
+        </Stack>
+        <Stack direction={"row"} justifyContent={"space-between"}>
+          <Typography>Venue</Typography>
+          <Typography>Dera Dari Restaurant C-12</Typography>
+        </Stack>
       </CardContent>
-      <CardActions>
-        <Button>RSVP</Button>
-      </CardActions>
+      <CardActions></CardActions>
     </Card>
   );
 };
